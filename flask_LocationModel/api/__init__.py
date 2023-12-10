196352671eff9b26aba9645b1e8191cbc83c06fc
Chamali Vishmani <chamali106astro@gmail.com>
2023-12-10 16:45:59 +0530

    fix apis except heat map

--- flask_LocationModel/api/__init__.py
+++ flask_LocationModel/api/__init__.py
@@ -5,18 +5,29 @@ from flask_socketio import SocketIO
 import joblib
 import pandas as pd
 import pymongo
+import urllib.parse
 
 from config.db_config import DB_URL
 
-client = pymongo.MongoClient(DB_URL)
+try:
+    client = pymongo.MongoClient(DB_URL)
+    print("Connected to MongoDB")
 
-mongoDb = client['insights']
-collection = mongoDb['heatmap_data']
+    mongoDb = client['insights']
+    collection = mongoDb['heatmap_data']
+    print("Connected to heatmap_data collection")
+    # print(
+    #     f"Number of documents in the collection: {collection.count_documents({})}")
+    # heatmap_data = list(collection.find({}).explain())
+    # heatmap_data = [print(doc) or doc for doc in collection.find({})]
 
-heatmap_data = list(collection.find({}))
-heatmap_data_df = pd.DataFrame(heatmap_data)
+    # print("Retrieved heatmap_data")
+    # heatmap_data_df = pd.DataFrame(heatmap_data)
+    # print("Created heatmap_data_df")
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 latitude_model = joblib.load("./models/lat_model.pkl")
 logitude_model = joblib.load("./models/lon_model.pkl")
 kmeans_model = joblib.load("./models/kmeans_model.pkl")
@@ -30,9 +41,9 @@ socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-    db.init_app(app)
+    # db.init_app(app)
     CORS(app)
 
     socketio.init_app(app, cors_allowed_origins="*")
--- flask_LocationModel/api/apis.py
+++ flask_LocationModel/api/apis.py
@@ -2,7 +2,9 @@ from flask import Blueprint, jsonify, request
 import joblib
 import pandas as pd
 
-from . import latitude_model, logitude_model, heatmap_data_df, hourcount_data, daycount_data, dayHrcount_data, kmeans_model, socketio
+from . import latitude_model, logitude_model, hourcount_data, daycount_data, dayHrcount_data, kmeans_model, socketio
+
+# heatmap_data_df
 
 main = Blueprint('main', __name__)
 
@@ -28,6 +30,31 @@ def predict_location_all():
         return jsonify({'error': str(e)}), 400
 
 
+# @main.route('/predict_location/closeLocations', methods=['POST'])
+# def predict_location_close():
+#     try:
+#         data = request.get_json()
+
+#         new_weekday = data['weekday']
+#         new_hour = data['hour']
+#         lat = data['lat']
+#         lon = data['lon']
+
+#         new_cluster = kmeans_model.predict([[lat, lon]])[0]
+#         # predicted_model = joblib.load(
+#         #     "./models/clusters/cluster_{}_model.pkl".format(new_cluster))
+#         # predicted_cluster = predicted_model.predict(
+#         #     [[new_weekday, new_hour]])[0]
+#         # cluster_centroid = kmeans_model.cluster_centers_[predicted_cluster]
+#         # predicted_latitude, predicted_longitude = cluster_centroid[0], cluster_centroid[1]
+
+#         response = {'predicted_lat': "predicted_latitude",
+#                     'predicted_lon': "predicted_longitude"}
+
+#         return jsonify(response), 200
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 400
+
 @main.route('/predict_location/closeLocations', methods=['POST'])
 def predict_location_close():
     try:
@@ -39,42 +66,44 @@ def predict_location_close():
         lon = data['lon']
 
         new_cluster = kmeans_model.predict([[lat, lon]])[0]
-        predicted_model = joblib.load(
-            "./models/clusters/cluster_{}_model.pkl".format(new_cluster))
-        predicted_cluster = predicted_model.predict(
-            [[new_weekday, new_hour]])[0]
-        cluster_centroid = kmeans_model.cluster_centers_[predicted_cluster]
-        predicted_latitude, predicted_longitude = cluster_centroid[0], cluster_centroid[1]
+        predicted_lat_model = joblib.load(
+            "./models/clusters/cluster_{}_lat_model.pkl".format(new_cluster))
+        predicted_lon_model = joblib.load(
+            "./models/clusters/cluster_{}_lon_model.pkl".format(new_cluster))
 
-        response = {'predicted_lat': predicted_latitude,
-                    'predicted_lon': predicted_longitude}
+        new_X = pd.DataFrame({'weekday': [new_weekday], 'hour': [new_hour]})
+        predicted_lat = predicted_lat_model.predict(new_X)
+        predicted_lon = predicted_lon_model.predict(new_X)
+
+        response = {'predicted_lat': predicted_lat.tolist(
+        ), 'predicted_lon': predicted_lon.tolist()}
 
         return jsonify(response), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
 
-@main.route('/heatmap_data', methods=['POST'])
-def get_heatmap_data():
-    try:
-        data = request.get_json()
+# @main.route('/heatmap_data', methods=['POST'])
+# def get_heatmap_data():
+#     try:
+#         data = request.get_json()
 
-        if 'weekday' not in data or 'hour' not in data:
-            return jsonify({'error': 'Missing parameters (weekday or hour)'}), 400
+#         if 'weekday' not in data or 'hour' not in data:
+#             return jsonify({'error': 'Missing parameters (weekday or hour)'}), 400
 
-        new_weekday = data['weekday']
-        new_hour = data['hour']
+#         new_weekday = data['weekday']
+#         new_hour = data['hour']
 
-        filtered_data = heatmap_data_df[(heatmap_data_df['weekday'] == new_weekday) & (
-            heatmap_data_df['hour'] == new_hour)]
+#         filtered_data = heatmap_data_df[(heatmap_data_df['weekday'] == new_weekday) & (
+#             heatmap_data_df['hour'] == new_hour)]
 
-        response = filtered_data[['Lat', 'Lon', 'weight']].to_dict(
-            orient='records')
+#         response = filtered_data[['Lat', 'Lon', 'weight']].to_dict(
+#             orient='records')
 
-        return jsonify({'heatmap_data': response}), 200
+#         return jsonify({'heatmap_data': response}), 200
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 400
 
 
 @main.route('/count_data/by_hour', methods=['GET'])
